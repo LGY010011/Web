3a2ec36242e9a1ce4877b0fd722988b33fa076d9
LGY010011 <gylm1111pm@gmail.com>
2024-11-03 00:47:22 +0900

    feat: 5주차 회원가입 유효성 검사 미션

--- MovieWeb/src/pages/signup.jsx
+++ MovieWeb/src/pages/signup.jsx
@@ -3,32 +3,46 @@ import useForm from '../hooks/useCustomForm';
 import * as yup from 'yup'
 import {yupResolver} from '@hookform/resolvers/yup'
 import styled from 'styled-components';
+import { validateSignup } from '../utils/validate';
 
 const SignupPage = () => {
-    const schema=yup.object().shape({
-        email: yup.string().email().required('이메일 입력은 필수입니다.'),
-        password: yup.string().min(8,'비밀번호는 8자 이상입니다.')
-                        .max(16,'비밀번호는 16자 이하입니다.').required(),
+    const signup=useForm({
+        initialValue: {
+            email:'',
+            password:'',
+            repassword:'',
+            birth:'',
+        },
+        validate: validateSignup
     })
 
-    const {register, handleSubmit, formState:{errors}}=useForm({
-        resolver: yupResolver(schema)
-    });
-
-    const onSubmit=(data)=>{
-        console.log('폼 제출')
-        console.log(data)
+    const handlePressSignup=()=>{
+        console.log(signup.values.email, signup.values.password, signup.values.repassword, signup.values.birth)
     }
+
+    // 회원가입 버튼 비활성화 조건
+    const isDisabled = !!signup.errors.email || !!signup.errors.password || !!signup.errors.repassword || !!signup.errors.birth ;
 
     return (
         <SignUpForm>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input type={'email'} placeholder='이메일을 입력하세요'{...register("email")}/>
-                <p>{errors.email?.message}</p>
-                <input type={'password'} placeholder='비밀번호를 입력하세요' {...register("password")}/>
-                <p>{errors.password?.message}</p>
-                <input type={'submit'}/>
-            </form>
+            <Title>회원가입</Title>
+            <Input error={signup.touched.email && signup.errors.email}type={'email'} placeholder={'이메일을 입력해주세요'} 
+                            {...signup.getTextInputProps('email')} />
+            {signup.touched.email && signup.errors.email && <ErrorText>{signup.errors.email}</ErrorText>}
+            
+            <Input  error={signup.touched.password && signup.errors.password} type={'password'} placeholder={'비밀번호를 입력해주세요'} 
+                            {...signup.getTextInputProps('password')} />
+            {signup.touched.password && signup.errors.password && <ErrorText>{signup.errors.password}</ErrorText>}
+
+            <Input  error={signup.touched.repassword && signup.errors.repassword} type={'password'} placeholder={'비밀번호를 다시 입력해주세요'} 
+                            {...signup.getTextInputProps('repassword')} />
+            {signup.touched.repassword && signup.errors.repassword && <ErrorText>{signup.errors.repassword}</ErrorText>}
+
+            <Input  error={signup.touched.birth && signup.errors.birth} type={'text'} placeholder={'생년월일을 입력하세요. YYYY.MM.DD'} 
+                            {...signup.getTextInputProps('birth')} />
+            {signup.touched.birth && signup.errors.birth && <ErrorText>{signup.errors.birth}</ErrorText>}
+
+            <Button onClick={handlePressSignup} type={'submit'} disabled={isDisabled} > 가입하기 </Button>
         </SignUpForm>
     );
 };
@@ -36,8 +50,49 @@ const SignupPage = () => {
 export default SignupPage;
 
 const SignUpForm=styled.div`
-    display:flex;
+    display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    height:100%;
+    margin: 30px;
+  
+`
+
+const Title=styled.h1`
+    color:white;
+`
+
+const Input=styled.input`
+    margin:10px 0;
+    padding: 0px;
+    width:300px;
+    height:45px;
+    border:1px solid #ccc;
+    border-radius:4px;
+
+    border: ${props=>(props.$error?'2px solid red':'1px solid #ccc')};
+
+    &:focus {
+        border-color:#007bff;
+    }
+`
+
+const ErrorText=styled.h1`
+    color:red;
+    font-size:12px;
+    text-align: left; 
+    width: 300px;
+`
+
+const Button=styled.button`
+    background-color: ${({ disabled }) => (disabled ? 'gray' : '#CD4275')};
+    font-size:15px;
+    width:300px;
+    height:45px;
+    margin-top: 30px;
+    border-radius:4px;
+    border:none;
+    padding: 10px;
+    box-sizing: border-box; /* padding을 포함하여 총 너비를 계산 */
+    color:white;
 `
